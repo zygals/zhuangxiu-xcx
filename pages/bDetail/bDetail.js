4acc30f5a1d1bf4e7c17a7e26831fbde9b1ad858
j28981 <2799024884@qq.com>
2017-11-02 03:02:11 +0800

    bdetail username gai

--- pages/bDetail/bDetail.js
+++ pages/bDetail/bDetail.js
@@ -11,176 +11,171 @@ Page({
 	 * 页面的初始数据
 	 */
 
-	data: {
+    data: {
 
-		inputNum: 1,
-		decrease: 'decrease',
-		imgurl: imgurl,
-		good: {},
-		username: '',
-		collect_star: 'collect-icon.png',
-    Collect: ""
-	},
+        inputNum: 1,
+        decrease: 'decrease',
+        imgurl: imgurl,
+        good: {},
+        username: '',
+        collect_star: 'collect-icon.png',
+        Collect: ""
+    },
 
 
 	/**
 	 * 生命周期函数--监听页面加载
 	 */
-	onLoad: function (options) {
-		var good_id = options.good_id;
-		// good_id = 19;
-		this.getGood(good_id)
-		var username = wx.getStorageSync('username');
+    onLoad: function (options) {
+        var good_id = options.good_id;
+        // good_id = 19;
+        this.getGood(good_id)
 
-	},
 
-	getGood: function (good_id) {
-		var that = this
-		var username = wx.getStorageSync('username')
-		if (!username) {
-			app.register()
-			username = wx.getStorageSync('username');
-		}
-		common.httpG('good/read', { good_id: good_id, username: username }, function (data) {
-			that.setData({ good: data.data })
+    },
 
-			if (data.is_collect == 'true') {
-				that.setData({
-          collect_star: 'collect-icon-slt.png',
-          Collect: 'isCollect'
-				})
+    getGood: function (good_id) {
+        var that = this
+        var username = common.getUserName()
+        common.httpG('good/read',
+            {
+                good_id: good_id, username: username
+            }, 
+            function (data) {
+                that.setData({ good: data.data })
+                if (data.is_collect == 'true') {
+                    that.setData({
+                        collect_star: 'collect-icon-slt.png',
+                        Collect: 'isCollect'
+                    })
 
-			}
+                }
 
-		})
-	},
-	decrease() {
-		var that = this;
-		var inputNum = that.data.inputNum;
-		if (inputNum > 1) {
-			inputNum--;
-		}
-		var decrease = inputNum <= 1 ? 'decrease' : '';
+            })
+    },
+    decrease() {
+        var that = this;
+        var inputNum = that.data.inputNum;
+        if (inputNum > 1) {
+            inputNum--;
+        }
+        var decrease = inputNum <= 1 ? 'decrease' : '';
 
-		that.setData({
-			inputNum: inputNum,
-			decrease: decrease
-		})
-	},
+        that.setData({
+            inputNum: inputNum,
+            decrease: decrease
+        })
+    },
 
-	increase() {
-		var that = this;
-		var inputNum = that.data.inputNum;
-		inputNum++;
-		var decrease = inputNum > 1 ? '' : 'decrease';
-		that.setData({
-			inputNum: inputNum,
-			decrease: decrease
-		})
+    increase() {
+        var that = this;
+        var inputNum = that.data.inputNum;
+        inputNum++;
+        var decrease = inputNum > 1 ? '' : 'decrease';
+        that.setData({
+            inputNum: inputNum,
+            decrease: decrease
+        })
 
-	},
+    },
 
-	placeChange(e) {
-		var that = this;
-		that.setData({
-			inputNum: e.detail.value
-		})
-	},
+    placeChange(e) {
+        var that = this;
+        that.setData({
+            inputNum: e.detail.value
+        })
+    },
 
-	//加入车  或  立即购买
-	tapAddCartGood: function (e) {
-		var good_id = e.target.dataset.good_id
-		var mai = e.target.dataset.mai
-		var username = common.getUserName()
+    //加入车  或  立即购买
+    tapAddCartGood: function (e) {
+        var good_id = e.target.dataset.good_id
+        var mai = e.target.dataset.mai
+        var username = common.getUserName()
 
-		var num = this.data.inputNum
-		common.httpP('cart/save', { username: username, good_id: good_id, num: num }, function (data) {
-			if (data.code == 0) {
-				wx.showToast({
-					title: data.msg,
-				})
-				if (mai == 'true') {
-					wx.switchTab({
-						url: '/pages/shopCart/shopCart',
-					})
-				}
-			}
-		})
-	},
-	//取商品
-	tapCollectGood: function (e) {
-		var that = this;
-		var good_id = e.currentTarget.dataset.good_id;
-		var username = wx.getStorageSync('username');
-		if (!username) {
-			app.register();
-			username = wx.getStorageSync('username')
-		}
-		common.httpG('collect/collect_good', {
-			username: username,
-			good_id: good_id
-		}, function (data) {
-			if (data.msg == '添加成功') {
-				that.setData({
-          collect_star: 'collect-icon-slt.png',
-          Collect: 'isCollect'
-				});
-			} else {
-				that.setData({
-					collect_star: 'collect-icon.png',
-           Collect: ''
-				});
+        var num = this.data.inputNum
+        common.httpP('cart/save', { username: username, good_id: good_id, num: num }, function (data) {
+            if (data.code == 0) {
+                wx.showToast({
+                    title: data.msg,
+                })
+                if (mai == 'true') {
+                    wx.switchTab({
+                        url: '/pages/shopCart/shopCart',
+                    })
+                }
+            }
+        })
+    },
+    //取商品
+    tapCollectGood: function (e) {
+        var that = this;
+        var good_id = e.currentTarget.dataset.good_id;
+        var username = common.getUserName()
+        common.httpG('collect/collect_good', {
+            username: username,
+            good_id: good_id
+        }, function (data) {
+            if (data.msg == '添加成功') {
+                that.setData({
+                    collect_star: 'collect-icon-slt.png',
+                    Collect: 'isCollect'
+                });
+            } else {
+                that.setData({
+                    collect_star: 'collect-icon.png',
+                    Collect: ''
+                });
 
-			}
-		});
-	},
+            }
+        });
+    },
 
 	/**
 	 * 生命周期函数--监听页面初次渲染完成
 	 */
-	onReady: function () {
+    onReady: function () {
 
-	},
+    },
 
 	/**
 	 * 生命周期函数--监听页面显示
 	 */
-	onShow: function () {
+    onShow: function () {
 
-	},
+    },
 
 	/**
 	 * 生命周期函数--监听页面隐藏
 	 */
-	onHide: function () {
+    onHide: function () {
 
-	},
+    },
 
 	/**
 	 * 生命周期函数--监听页面卸载
 	 */
-	onUnload: function () {
+    onUnload: function () {
 
-	},
+    },
 
 	/**
 	 * 页面相关事件处理函数--监听用户下拉动作
 	 */
-	onPullDownRefresh: function () {
+    onPullDownRefresh: function () {
 
-	},
+    },
 
 	/**
 	 * 页面上拉触底事件的处理函数
 	 */
-	onReachBottom: function () {
+    onReachBottom: function () {
 
-	},
+    },
 
 	/**
 	 * 用户点击右上角分享
 	 */
-	onShareAppMessage: function () {
+    onShareAppMessage: function () {
 
-	}
+    }
 })
